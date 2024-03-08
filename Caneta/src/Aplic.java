32f7045f90c46786baa6d3959eb148a7729d441f
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-03-08 06:10:08 -0300

    add exercicio poo caneta

---
+++ Caneta/src/Aplic.java
@@ -0,0 +1,21 @@
+
+import fatec.poo.model.Caneta;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author blossom
+ */
+public class Aplic {
+
+    public static void main(String[] args) {
+        Caneta caneta1 = new Caneta("NIC", 0.4f, "Amarelo" , false);
+        caneta1.status();
+    }
+    
+}
