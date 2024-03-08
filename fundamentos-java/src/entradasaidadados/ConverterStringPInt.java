3a8837e9a8e5bd50edddcc81d95b94cb34daad7a
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-03-08 06:12:41 -0300

    add exercicio casting

---
+++ fundamentos-java/src/entradasaidadados/ConverterStringPInt.java
@@ -0,0 +1,16 @@
+
+package entradasaidadados;
+
+/**
+ *
+ * @author Grazi
+ */
+public class ConverterStringPInt {
+       public static void main(String[] args) {
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
+    }
+
+} 
+
